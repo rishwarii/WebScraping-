688b705564fb6f92cf8f1275d8ffbc32c5aacd77
rishwari <rihwariranjan@gmail.com>
2021-07-23 02:22:33 +0530

    sort by date
    few keyword

---
+++ PythonJobScraping/main.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+urljob = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation='
+html_text = requests.get(urljob).text
+
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+for job in jobs:
+    published_date = job.find(
+        'span', class_='sim-posted').span.text.replace(' ', '')
+    if 'few' in published_date:
+        company_name = job.find(
+            'h3',  class_='joblist-comp-name').text.replace(' ', '')
+
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+
+    print(f'''
+    Company Name : {company_name}
+    Required Skills : {skills}
+    ''')
+    print('-------------')
+# Date Posted: {published_date}
